9562d9b6088c9fca667c3774e12fd79f3303b6e2
Charles Wang <cwang@realpractice.com>
2011-09-25 23:03:44 -0700

    initial commit with the postgres setup as DB

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ossc::Application.config.secret_token = '06b934dc295f5d206a70199efe8af5d7fe5a26d8d5cd95307e9be9b2f5965148a499da8c802e7810de92f5fff8df8257793dcb39596f1a20b73363b1030d62d0'
