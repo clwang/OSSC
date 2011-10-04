61754220e1689fe7eba06efb6053f6c5f7aeb1e6
Charles Wang <cwang@realpractice.com>
2011-10-03 18:17:47 -0700

    got the devise redirects working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  private
+  
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+  
+  def after_sign_in_path_for(resource)
+      stored_location_for(resource) || '/dashboard'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Ossc::Application.routes.draw do
   devise_for :users
 
   get "home/index"
+  
+  match '/dashboard' => "dashboard#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
