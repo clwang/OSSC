44f6e0c750310c57b94202c9c015ad0f6dbca9de
Charles Wang <cwang@realpractice.com>
2011-10-09 00:45:13 -0700

    updated the task migration and created relation between tasks and project

--- db/migrate/20111009073253_create_tasks.rb
+++ db/migrate/20111009073253_create_tasks.rb
@@ -1,7 +1,10 @@
 class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
-
+      t.text :description
+      t.integer :point_value
+      t.string :status
+      t.integer :project_id
       t.timestamps
     end
   end
