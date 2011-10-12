ee160e5c6f2c1272dc091cb0b65dfc5635862e46
Charles Wang <cwang@realpractice.com>
2011-10-12 14:34:57 -0700

    added admin column to the user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111012211126) do
+ActiveRecord::Schema.define(:version => 20111012213240) do
 
   create_table "projects", :force => true do |t|
     t.datetime "created_at"
@@ -53,8 +53,8 @@ ActiveRecord::Schema.define(:version => 20111012211126) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "email",                                 :default => "",    :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(:version => 20111012211126) do
     t.datetime "updated_at"
     t.string   "nickname"
     t.string   "uid"
+    t.boolean  "admin",                                 :default => false
   end
 
   add_index "users", ["authentication_token"], :name => "index_users_on_authentication_token", :unique => true
